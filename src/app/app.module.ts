54b23d4bc39618a91f6cf1220a785919340c21e6
unknown <dbrwhdgh1203@naver.com>
2022-07-27 09:23:00 +0900

    main screen design update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { NbEvaIconsModule } from '@nebular/eva-icons';
 import { NbButtonModule, NbCardModule, NbIconModule, NbLayoutModule, NbSelectModule, NbSidebarModule, NbThemeModule } from '@nebular/theme'
 import { typeComponent } from './layout/type.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    typeComponent
+    typeComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
