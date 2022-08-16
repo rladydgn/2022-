36b2038b52b88543864e3ff801da2522d2375480
unknown <dbrwhdgh1203@naver.com>
2022-08-17 00:58:57 +0900

    build error resolve

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  warUrl: 'http://121.129.12.192:33333/base/fightRecord/',
+  termUrl: 'http://121.129.12.192:33333/milltaryTerm/',
+  quizUrl: 'http://121.129.12.192:33333/base/quiz/?category=militaryTerm',
+  quizUrl2: 'http://121.129.12.192:33333/base/quiz/?category=fightRecord',
+  quizUrl3: 'http://121.129.12.192:33333/base/quiz/?category=all'
 };
